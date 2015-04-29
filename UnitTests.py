f8f4bf6962d10ed8795e510842592b93bd4d6098
CGCooke <Cameron.G.Cooke@mail.com>
2015-04-30 09:16:21 +1000

    Added another unit test
    Unit test checks that XYZ2LLH properly projects point in ECEF to LLH

--- UnitTests.py
+++ UnitTests.py
@@ -2,10 +2,56 @@ import unittest
 import numpy as np
 import random
 import time
+import os
 
 import geoidReader
 
 class TestSequenceFunctions(unittest.TestCase):
+	def test_XYZ2LLH(self):
+
+		#Create a test data file 
+		f = open('test.csv','w')
+		f.write('-4657740.9452499999,2550059.4295000001,-3521355.9040000001\n')
+		f.write('-4657740.9452499999,2550059.4295000001,-3521355.9040000001\n')
+		f.close()
+
+		#Run it through XYZ2LLH
+		os.system('python XYZ2LLH.py  -i  test.csv -o testOut.csv')
+		
+		#Check the results
+		inputData = np.loadtxt('testOut.csv',delimiter=',',usecols=(0,1,2))
+		lon=inputData[0,0]
+		lat=inputData[0,1]
+		height=inputData[0,2]
+		np.testing.assert_allclose([lon,lat,height],[151.299804,-33.727470,18.7184])
+
+		#Clean up
+		os.system('rm test.csv')
+		os.system('rm testOut.csv')
+
+	'''
+	def test_Reproject(self):
+
+		#Create a test data file 
+		f = open('test.csv','w')
+		f.write('151.30518782,-33.70769934,22.216\n')
+		f.write('151.30518782,-33.70769934,22.216\n')
+		f.close()
+
+		#Run it through XYZ2LLH
+		os.system('python Reproject.py -i test.csv -o testOut.csv --epsgCode epsg:32756')
+		
+		#Check the results
+		inputData = np.loadtxt('testOut.csv',delimiter=',',usecols=(0,1,2))
+		lon=inputData[0,0]
+		lat=inputData[0,1]
+		height=inputData[0,2]
+		np.testing.assert_allclose([lon,lat,height],[151.299804,-33.727470,18.7184])
+
+		#Clean up
+		os.system('rm test.csv')
+		os.system('rm testOut.csv')
+	'''
 
 	def test_computePointOffset(self):
 		#Heights are being checked against Geoscience Australia Applet
