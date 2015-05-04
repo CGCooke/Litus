07fd8dc7a61db7e06f1c684669a640ef4f75153e
CGCooke <Cameron.G.Cooke@mail.com>
2015-05-04 13:59:07 +1000

    Changed Filter to deal with lack of polygon

--- Filter.py
+++ Filter.py
@@ -1,11 +1,17 @@
+''' Style Guide http://www.python.org/dev/peps/pep-0008/ '''
+
+''' standard library imports '''
+import time
+
+''' related third party imports ''' 
 import numpy as np
 from optparse import OptionParser
 from shapely.geometry import Polygon,Point
 from shapely.prepared import prep
 
-import time
+''' local application/library specific imports '''
 
-def filter(options):
+def Filter(options):
 	'''
 	Rounding
 	http://gis.stackexchange.com/questions/8650/how-to-measure-the-accuracy-of-latitude-and-longitude
@@ -18,20 +24,23 @@ def filter(options):
 	lat=inputData[:,1]
 	height=inputData[:,2]
 
-	poly = np.loadtxt('Data/Common/polygon.txt',delimiter=',', usecols=(1,2))
-	poly = Polygon(poly)
-	poly = prep(poly)
-	
+	if options.polyFilter != False:
+		poly = np.loadtxt(options.polyFilter,delimiter=',', usecols=(1,2))
+		poly = Polygon(poly)
+		poly = prep(poly)
+		
 	minHeight = float(options.minHeight)
 	maxHeight = float(options.maxHeight)
 	numPoints = inputData.shape[0]
 
 	for i in range(0,numPoints):
 		pointLon,pointLat,pointHeight = lon[i],lat[i],height[i]
-		point = Point(pointLon,pointLat)
 		if minHeight < pointHeight < maxHeight:	
-			if poly.contains(point):	
-				#Rounding down to save on space and improve I/O
+			if options.polyFilter != False:
+				point = Point(pointLon,pointLat)
+				if poly.contains(point):
+					outputData.write(str(round(pointLon,8))+','+str(round(pointLat,8))+','+str(round(pointHeight,4))+'\n')
+			else:
 				outputData.write(str(round(pointLon,8))+','+str(round(pointLat,8))+','+str(round(pointHeight,4))+'\n')
 	outputData.close()
 	
@@ -45,10 +54,9 @@ if __name__ == '__main__':
 	parser.add_option('-o','--outputFilePath', action="store", dest="outputFileName",help="Path to the output file")
 	parser.add_option('--minHeight', action="store",  dest='minHeight',help="Minimum height threshold for points")
 	parser.add_option('--maxHeight', action="store",  dest='maxHeight',help="Maximum height threshold for points")
-	parser.add_option('--polygon', dest='polyFilter',default=False, action='store_true',help="Path to the polygon csv file")
-
+	parser.add_option('--polygon', dest='polyFilter',default=False, help="Path to the polygon csv file")
 
 	(options, args) = parser.parse_args()
-	filter(options)
+	Filter(options)
 
 
